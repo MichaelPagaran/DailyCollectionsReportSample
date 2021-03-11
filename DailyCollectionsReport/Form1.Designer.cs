345a47ef0e3ac7a7aac577d9b08204b6026a876c
Michael Pagaran <mmpagaran64@gmail.com>
2021-03-11 15:43:54 +0800

    First and final commit

---
+++ DailyCollectionsReport/Form1.Designer.cs
@@ -0,0 +1,320 @@
+﻿
+namespace DailyCollectionsReport {
+    partial class DailyCollectionReportForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.topPanel = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.mainTopLabel = new System.Windows.Forms.Label();
+            this.bottomPanel = new System.Windows.Forms.Panel();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.midPanel = new System.Windows.Forms.Panel();
+            this.mainDataGrid = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.mySeparator2 = new DailyCollectionsReport.MySeparator();
+            this.mySeparator1 = new DailyCollectionsReport.MySeparator();
+            this.printButton = new System.Windows.Forms.Button();
+            this.exportButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.topPanel.SuspendLayout();
+            this.bottomPanel.SuspendLayout();
+            this.midPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.mainDataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // topPanel
+            // 
+            this.topPanel.Controls.Add(this.button1);
+            this.topPanel.Controls.Add(this.label2);
+            this.topPanel.Controls.Add(this.label1);
+            this.topPanel.Controls.Add(this.mySeparator1);
+            this.topPanel.Controls.Add(this.mainTopLabel);
+            this.topPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.topPanel.Location = new System.Drawing.Point(0, 0);
+            this.topPanel.Name = "topPanel";
+            this.topPanel.Padding = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.topPanel.Size = new System.Drawing.Size(835, 124);
+            this.topPanel.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label2.Location = new System.Drawing.Point(121, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "3/2/21";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label1.Location = new System.Drawing.Point(13, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Report for Date:";
+            // 
+            // mainTopLabel
+            // 
+            this.mainTopLabel.AutoSize = true;
+            this.mainTopLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mainTopLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(58)))), ((int)(((byte)(108)))));
+            this.mainTopLabel.Location = new System.Drawing.Point(12, 28);
+            this.mainTopLabel.Name = "mainTopLabel";
+            this.mainTopLabel.Size = new System.Drawing.Size(199, 24);
+            this.mainTopLabel.TabIndex = 1;
+            this.mainTopLabel.Text = "Daily Collection Report";
+            // 
+            // bottomPanel
+            // 
+            this.bottomPanel.Controls.Add(this.exportButton);
+            this.bottomPanel.Controls.Add(this.printButton);
+            this.bottomPanel.Controls.Add(this.label10);
+            this.bottomPanel.Controls.Add(this.mySeparator2);
+            this.bottomPanel.Controls.Add(this.label9);
+            this.bottomPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.bottomPanel.ForeColor = System.Drawing.Color.White;
+            this.bottomPanel.Location = new System.Drawing.Point(0, 591);
+            this.bottomPanel.Name = "bottomPanel";
+            this.bottomPanel.Padding = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.bottomPanel.Size = new System.Drawing.Size(835, 124);
+            this.bottomPanel.TabIndex = 1;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(58)))), ((int)(((byte)(108)))));
+            this.label10.Location = new System.Drawing.Point(722, 11);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(90, 24);
+            this.label10.TabIndex = 7;
+            this.label10.Text = "1,123,123";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(58)))), ((int)(((byte)(108)))));
+            this.label9.Location = new System.Drawing.Point(580, 11);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(61, 24);
+            this.label9.TabIndex = 1;
+            this.label9.Text = "Total: ";
+            // 
+            // midPanel
+            // 
+            this.midPanel.Controls.Add(this.mainDataGrid);
+            this.midPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.midPanel.Location = new System.Drawing.Point(0, 124);
+            this.midPanel.Name = "midPanel";
+            this.midPanel.Padding = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.midPanel.Size = new System.Drawing.Size(835, 467);
+            this.midPanel.TabIndex = 2;
+            // 
+            // mainDataGrid
+            // 
+            this.mainDataGrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.mainDataGrid.BackgroundColor = System.Drawing.Color.White;
+            this.mainDataGrid.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
+            this.mainDataGrid.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(58)))), ((int)(((byte)(108)))));
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.mainDataGrid.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.mainDataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.mainDataGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6});
+            this.mainDataGrid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainDataGrid.GridColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.mainDataGrid.Location = new System.Drawing.Point(10, 0);
+            this.mainDataGrid.Name = "mainDataGrid";
+            this.mainDataGrid.Size = new System.Drawing.Size(815, 467);
+            this.mainDataGrid.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "#";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Receipt No.";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Payment Method";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Description";
+            this.Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Amount";
+            this.Column5.Name = "Column5";
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Cashier";
+            this.Column6.Name = "Column6";
+            // 
+            // mySeparator2
+            // 
+            this.mySeparator2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.mySeparator2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(58)))), ((int)(((byte)(108)))));
+            this.mySeparator2.IsVertical = false;
+            this.mySeparator2.Location = new System.Drawing.Point(17, 39);
+            this.mySeparator2.Name = "mySeparator2";
+            this.mySeparator2.Size = new System.Drawing.Size(808, 23);
+            this.mySeparator2.TabIndex = 2;
+            this.mySeparator2.Text = "mySeparator2";
+            this.mySeparator2.Thickness = 3;
+            // 
+            // mySeparator1
+            // 
+            this.mySeparator1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.mySeparator1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(58)))), ((int)(((byte)(108)))));
+            this.mySeparator1.IsVertical = false;
+            this.mySeparator1.Location = new System.Drawing.Point(16, 55);
+            this.mySeparator1.Name = "mySeparator1";
+            this.mySeparator1.Size = new System.Drawing.Size(806, 23);
+            this.mySeparator1.TabIndex = 2;
+            this.mySeparator1.Text = "mySeparator1";
+            this.mySeparator1.Thickness = 3;
+            // 
+            // printButton
+            // 
+            this.printButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.printButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(58)))), ((int)(((byte)(108)))));
+            this.printButton.FlatAppearance.BorderSize = 0;
+            this.printButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.printButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.printButton.Location = new System.Drawing.Point(725, 68);
+            this.printButton.Name = "printButton";
+            this.printButton.Size = new System.Drawing.Size(96, 34);
+            this.printButton.TabIndex = 8;
+            this.printButton.Text = "Print";
+            this.printButton.UseVisualStyleBackColor = false;
+            // 
+            // exportButton
+            // 
+            this.exportButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.exportButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(58)))), ((int)(((byte)(108)))));
+            this.exportButton.FlatAppearance.BorderSize = 0;
+            this.exportButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.exportButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exportButton.Location = new System.Drawing.Point(584, 68);
+            this.exportButton.Name = "exportButton";
+            this.exportButton.Size = new System.Drawing.Size(111, 34);
+            this.exportButton.TabIndex = 9;
+            this.exportButton.Text = "Export To CSV";
+            this.exportButton.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(788, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(33, 21);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // DailyCollectionReportForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(835, 715);
+            this.Controls.Add(this.midPanel);
+            this.Controls.Add(this.bottomPanel);
+            this.Controls.Add(this.topPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "DailyCollectionReportForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.topPanel.ResumeLayout(false);
+            this.topPanel.PerformLayout();
+            this.bottomPanel.ResumeLayout(false);
+            this.bottomPanel.PerformLayout();
+            this.midPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.mainDataGrid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel topPanel;
+        private System.Windows.Forms.Label mainTopLabel;
+        private MySeparator mySeparator1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel bottomPanel;
+        private MySeparator mySeparator2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Panel midPanel;
+        private System.Windows.Forms.DataGridView mainDataGrid;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.Button exportButton;
+        private System.Windows.Forms.Button printButton;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ DailyCollectionsReport/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DailyCollectionsReport {
+    public partial class DailyCollectionReportForm : Form {
+        public DailyCollectionReportForm() {
+            InitializeComponent();
+        }
+
+        /** Make windows movable
+        private bool mouseDown;
+        private Point lastLocation;
+        private void Form1_MouseDown(object sender, MouseEventArgs e) {
+            mouseDown = true;
+            lastLocation = e.Location;
+        }
+        private void Form1_MouseMove(object sender, MouseEventArgs e) {
+            if (mouseDown) {
+                this.Location = new Point((this.Location.X - lastLocation.X) + e.X, (this.Location.Y - lastLocation.Y) + e.Y);
+                this.Update();
+            }
+        }
+        private void Form1_MouseUp(object sender, MouseEventArgs e) {
+            mouseDown = false;
+        }
+        **/
+    }
+
+    class MySeparator : Control { //This is the 'line' graphics. Like <hr> in HTML
+        private int thickness = 1;
+        private bool isVertical; 
+
+        protected override void OnPaint(PaintEventArgs e) {
+            base.OnPaint(e);
+            var size = isVertical ? Height / 2 : Width / 2;
+            e.Graphics.TranslateTransform(Width / 2f, Height / 2f);
+
+            using (var pen = new Pen(ForeColor, thickness)) {
+                if (!isVertical) {
+                    e.Graphics.DrawLine(pen, -size + Padding.Left, 0, size - Padding.Right, 0);
+                } else {
+                    e.Graphics.DrawLine(pen, 0, -size + Padding.Top, 0, size - Padding.Bottom);
+                }
+            }
+        }
+        protected override void OnPaddingChanged(EventArgs e) {
+            base.OnPaddingChanged(e);
+            Invalidate();
+        }
+        public bool IsVertical {
+            get => isVertical;
+            set {
+                isVertical = value;
+                Invalidate();
+            }
+        }
+        public int Thickness {
+            get => thickness;
+            set {
+                thickness = value;
+                if(Height < thickness) {
+                    Height = thickness;
+                }else {
+                    Invalidate();
+                }
+            }
+        }
+    }
+}
